58e90e4e95316143605c819e2bd89c97b5a19e13
Naominm <naomimbugua536@gmail.com>
2025-03-24 15:35:41 +0300

    Refine the about section

--- src/pages/Home/About.tsx
+++ src/pages/Home/About.tsx
@@ -1,5 +1,11 @@
-import { ReactNode } from 'react';
+
 import AboutImage from "../../assets/mutc-images/hero2.jpg"
+import FlatImage from "../../assets/mutc-images/mission.png"
+import FlatImage2 from "../../assets/mutc-images/value.png"
+import FlatImage3 from "../../assets/mutc-images/objective.png"
+
+import { MdGroups2 } from "react-icons/md";
+import { LiaAwardSolid } from "react-icons/lia";
 
 import "./About.css"
 
@@ -8,13 +14,14 @@ import "./About.css"
     TextContent:String;
  }
  interface VisionMisionProps{
-    FlatIcon:ReactNode;
+    FlatIcon:string;
     Text:string;
+    description:string;
  }
 function AboutSection() {
     return ( 
         <section className="about-section">
-            <h1>Mut Tech Club Is The Top Tech Club In Muranga University</h1>
+            <h1>Mut Tech Club </h1>
             <AboutSectionContent Image={AboutImage} TextContent=" TechClub stands as the premier global IT solution provider, delivering cutting-edge services and unmatched expertise to meet the diverse needs of industries worldwide." />
         </section>
      );
@@ -32,27 +39,46 @@ function AboutSectionContent({Image, TextContent}:AboutSectionContentProps){
   <div className="text-container">
   <p>{TextContent}</p>
   <div className="sec-2">
-  <VisionMision FlatIcon='' Text='members'/>
-  <VisionMision FlatIcon='' Text='Tracks '/>
+    <div className="member">
+    <h1> <MdGroups2/> </h1>
+    <span> 200+ Members</span>
+   
+    
+    </div>
+<div className="award">
+<h1><LiaAwardSolid/></h1>
+<span>5+ Awards</span>
+
+
+</div>
+
+ 
   </div>
+  <div className="btn-class">
+  <button className="btn1">Join us</button>
+  </div>
+ 
   </div>
   
         </div>
         <div className="text-container-2">
-    <VisionMision FlatIcon='' Text='our mission'/>
-    <VisionMision FlatIcon='' Text='values'/>
-    <VisionMision FlatIcon='' Text='objective '/>
+    <VisionMision FlatIcon={FlatImage} Text='our mission'description='delivering cutting-edge services and unmatched expertise to meet the diverse needs'/>
+    <VisionMision FlatIcon={FlatImage2} Text='values'description='delivering cutting-edge services and unmatched expertise to meet the diverse needs'/>
+    <VisionMision FlatIcon={FlatImage3} Text='objective ' description='delivering cutting-edge services and unmatched expertise to meet the diverse needs'/>
   </div>
         </div>
     )
 }
 
-function VisionMision({FlatIcon,Text}:VisionMisionProps){
+function VisionMision({FlatIcon,Text,description}:VisionMisionProps){
     return(
         <div className="parent-v">
             <div className="m-v-containers">
-            <p>{FlatIcon}</p>
-            <p>{Text}</p>
+                <div className="flat-image-wrapper">
+                    <img src={FlatIcon} alt="" />
+                </div>
+            <p className="text-c">{Text}</p>
+            <p className='text-desc-m-v'>{description}</p>
             </div>
        
         </div>
