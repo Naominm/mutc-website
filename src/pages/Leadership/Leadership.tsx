831e8763b5584c12bed7f2ce2571370b1cfcf535
Naominm <naomimbugua536@gmail.com>
2025-03-24 18:27:45 +0300

    Build The card Section

--- src/pages/Leadership/Leadership.tsx
+++ src/pages/Leadership/Leadership.tsx
@@ -1,4 +1,4 @@
-import CardContent from "../../components/card/Card";
+
 import video from "../../assets/mutc-images/team.mp4"
 import "./Leadership.css"
 
@@ -11,13 +11,31 @@ interface LeadershipContentsProps{
 type  leadershipContainerProps={
     Text:string;
 }
+interface CardProps {
+    user: string;
+    name: string;
+    description: string;
+    text: string;
+}
 function Leadership() {
     return ( 
         <div className="leadership-parent-container">
            <LeadershipContents Heading="Meet the Team" Text='Mutc has dedicated software developes committed to success' Video={video}/> 
-        <div className="leadership-section">
-            <Contleadership Text='Leaders'/>
-            <CardComponent/>
+        <div className="leadership-section-cards">
+            <div className="cards">
+            < CardComponent 
+                name="John Doe"
+                user="/path/to/image1.jpg"
+                description="Lead Developer"
+                text="Innovation & Creativity"
+            />
+            < CardComponent 
+                user="/path/to/image2.jpg"
+                name="Jane Smith"
+                description="UI/UX Designer"
+                text="Crafting Experiences"
+            />
+        </div>
         </div>
         </div>
      );
@@ -39,21 +57,25 @@ function LeadershipContents({Heading, Text, Video}:LeadershipContentsProps){
     )
 }
 
-function Contleadership({Text}:leadershipContainerProps){
-    return(
-        <div className="container-element">
-              
-              <p className="border"></p>
-            <h2 className="heading">{Text}</h2>
-        </div>
-    )
-}
 
-function CardComponent(){
+function CardComponent({ user, name, description, text }: CardProps){
 return(
-    <div>
-       <CardContent user="" name='' description="" text=""/>
-    </div>
+    <div className="card">
+            <div className="card-inner">
+              
+                <div className="card-front">
+                <h3>{name}</h3>
+                    <img src={user} alt="User" className="user-image" />
+           
+                </div>
+
+             
+                <div className="card-back">
+                    <p>{description}</p>
+                    <span>{text}</span>
+                </div>
+            </div>
+        </div>
 )
 }
 
---
+++ src/store/cardStore.js
@@ -0,0 +1,11 @@
+
+import {create} from 'zustand'
+function CardStore(set){
+    return{
+ card:[],
+ 
+    }
+}
+
+ const  useCardStore=create(MutcStore());
+ export default useCardStore;
